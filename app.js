f503431cd665c05ce21ac886db84497be734362a
devonhackley <devonhackley@gmail.com>
2016-10-24 14:29:33 -0700

    More javascript

--- app.js
+++ app.js
@@ -5,8 +5,48 @@ var pike = {
   maxHourlyCustomers:65,
   averageCookies:6.3,
   randomNumCustomer: function (){
-    Math.floor(Math.random() * 65) + 23;
-  }
-  
+    Math.floor(Math.random() * maxHourlyCustomers) + minHourlyCustomers;
+  },
+  results:[],
 
+};
+
+var airport = {
+  minHourlyCustomers:3,
+  maxHourlyCustomers:24,
+  averageCookies:1.2,
+  randomNumCustomer: function (){
+    Math.floor(Math.random() * maxHourlyCustomers) + minHourlyCustomers;
+  },
+  results:[],
+};
+
+var center = {
+  minHourlyCustomers:11,
+  maxHourlyCustomers:38,
+  averageCookies:3.7,
+  randomNumCustomer: function (){
+    Math.floor(Math.random() * maxHourlyCustomers) + minHourlyCustomers;
+  },
+  results:[],
+};
+
+var hill = {
+  minHourlyCustomers:20,
+  maxHourlyCustomers:38,
+  averageCookies:2.3,
+  randomNumCustomer: function (){
+    Math.floor(Math.random() * maxHourlyCustomers) + minHourlyCustomers;
+  },
+  results:[],
+};
+
+var alki = {
+  minHourlyCustomers:2,
+  maxHourlyCustomers:16,
+  averageCookies:4.6,
+  randomNumCustomer: function (){
+    Math.floor(Math.random() * maxHourlyCustomers) + minHourlyCustomers;
+  },
+  results:[],
 };
