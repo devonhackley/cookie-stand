d2a534f32d965859b0de635ef028f94a4717b65a
devonhackley <devonhackley@gmail.com>
2016-10-25 09:56:52 -0700

    FINALLY understand what we are doing lol...somewhat

--- app.js
+++ app.js
@@ -1,57 +1,156 @@
 'use strict';
 
+var hours = ['6am','7am','8am','9am','10am','11am','12pm','1pm','2pm','3pm','4pm','5pm','6pm','7pm'];
+
 var pike = {
+  name:'1st & Pike',
   minHourlyCustomers:23,
   maxHourlyCustomers:65,
   averageCookies:6.3,
-  randomNumCustomer: function (){
-    Math.floor(Math.random() * maxHourlyCustomers) + minHourlyCustomers;
-  },
-  cookiesPurchased: randomNumCustomer() * averageCookies,
-  results:[],
+  dailySales:[],
 
+  randomNumCustomer: function (){
+    return Math.floor(Math.random() * (this.maxHourlyCustomers - this.minHourlyCustomers) + this.minHourlyCustomers);
+  },
+
+  calcCookiesSales: function () {
+    for (var i = 0 ; i < hours.length;i++) {
+      this.dailySales[i] = Math.floor(this.randomNumCustomer() * this.averageCookies);
+    }
+  },
+
+  renderSales: function() {
+    //calculate cookies
+    this.calcCookiesSales();
+
+    //attaching store name
+    var h2 = document.createElement('h2');
+    h2.innerText(this.name);
+    var main = document.getElementByTagName('main')[0];
+    main.appendChild(h2);
+
+    //attaching li to list
+    var ul = document.createElement('ul');
+    for (var i = 0; i < hours.length; i++) {
+      var li = document.createElement('li');
+      li.innerText = hours[i] + ': ' + this.dailySales[i] + ' cookies';
+      ul.appendChild(li);
+    }
+
+    //attaching total sales to daily sales
+    var totalSales = 0;
+    for (var i = 0; i < this.dailySales; i++) {
+      totalSales += this.dailySales[i];
+    }
+    var li = document.createElement('li');
+    li.innerText = 'Total Sales: ' + totalSales + ' cookies';
+
+    //Append ul to the dom
+    main.appendChild(ul);
+  }
 };
 
 var airport = {
+  name:'seatac-airport',
   minHourlyCustomers:3,
   maxHourlyCustomers:24,
   averageCookies:1.2,
+  dailySales:[],
+
   randomNumCustomer: function (){
-    Math.floor(Math.random() * maxHourlyCustomers) + minHourlyCustomers;
+    return Math.floor(Math.random() * (this.maxHourlyCustomers - this.minHourlyCustomers) + this.minHourlyCustomers);
   },
-  cookiesPurchased: randomNumCustomer() * averageCookies,
-  results:[],
+
+  calcCookiesSales: function () {
+    for (var i = 0 ; i < hours.length;i++) {
+      this.dailySales[i] = Math.floor(this.randomNumCustomer() * this.averageCookies);
+    }
+  },
+
+  renderSales: function() {
+    //calculate cookies
+    this.calcCookiesSales();
+
+    //attaching store name
+    var h2 = document.createElement('h2');
+    h2.innerText(this.name);
+    var main = document.getElementByTagName('main')[0];
+    main.appendChild(h2);
+
+    //attaching li to list
+    var ul = document.createElement('ul');
+    for (var i = 0; i < hours.length; i++) {
+      var li = document.createElement('li');
+      li.innerText = hours[i] + ': ' + this.dailySales[i] + ' cookies';
+      ul.appendChild(li);
+    }
+    //attaching total sales to daily sales
+    var totalSales = 0;
+    for (var i = 0; i < this.dailySales; i++) {
+      totalSales += this.dailySales[i];
+    }
+    var li = document.createElement('li');
+    li.innerText = 'Total Sales: ' + totalSales + ' cookies';
+
+    //Append ul to the dom
+    main.appendChild(ul);
+  }
 };
 
-var center = {
-  minHourlyCustomers:11,
-  maxHourlyCustomers:38,
-  averageCookies:3.7,
-  randomNumCustomer: function (){
-    Math.floor(Math.random() * maxHourlyCustomers) + minHourlyCustomers;
-  },
-  cookiesPurchased:randomNumCustomer() * averageCookies,
-  results:[],
-};
+var hill = function () {
+  name:'Capitol Hill',
+  minHourlyCustomers:23,
+  maxHourlyCustomers:65,
+  averageCookies:6.3,
+  dailySales:[],
 
-var hill = {
-  minHourlyCustomers:20,
-  maxHourlyCustomers:38,
-  averageCookies:2.3,
   randomNumCustomer: function (){
-    Math.floor(Math.random() * maxHourlyCustomers) + minHourlyCustomers;
+    return Math.floor(Math.random() * (this.maxHourlyCustomers - this.minHourlyCustomers) + this.minHourlyCustomers);
   },
-  cookiesPurchased:randomNumCustomer() * averageCookies,
-  results:[],
-};
 
-var alki = {
-  minHourlyCustomers:2,
-  maxHourlyCustomers:16,
-  averageCookies:4.6,
-  randomNumCustomer: function (){
-    Math.floor(Math.random() * maxHourlyCustomers) + minHourlyCustomers;
+  calcCookiesSales: function () {
+    for (var i = 0 ; i < hours.length;i++) {
+      this.dailySales[i] = Math.floor(this.randomNumCustomer() * this.averageCookies);
+    }
   },
-  cookiesPurchased:randomNumCustomer() * averageCookies,
-  results:[],
-};
+
+  renderSales: function() {
+    //calculate cookies
+    this.calcCookiesSales();
+
+    //attaching store name
+    var h2 = document.createElement('h2');
+    h2.innerText(this.name);
+    var main = document.getElementByTagName('main')[0];
+    main.appendChild(h2);
+
+    //attaching li to list
+    var ul = document.createElement('ul');
+    for (var i = 0; i < hours.length; i++) {
+      var li = document.createElement('li');
+      li.innerText = hours[i] + ': ' + this.dailySales[i] + ' cookies';
+      ul.appendChild(li);
+    }
+
+    //attaching total sales to daily sales
+    var totalSales = 0;
+    for (var i = 0; i < this.dailySales; i++) {
+      totalSales += this.dailySales[i];
+    }
+    var li = document.createElement('li');
+    li.innerText = 'Total Sales: ' + totalSales + ' cookies';
+
+    //Append ul to the dom
+    main.appendChild(ul);
+
+}
+
+//adding elements to html using javascript
+// var h1 = document.createElement('h1'); //create
+// h1.innerText = 'All Cookie Stores'; //add data
+// var main = document.getElementByTagName('main')[0]; //attach to new element
+// main.appendChild(h1);
+
+
+//function calls
+pike.renderSales();
